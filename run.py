66acb929e1704dbc2c3b5b262d5a4f0a38a00827
SRay-2020 <stephen.r.ray@gmail.com>
2021-05-27 15:28:33 +0000

    Calculate surplus data, zip method

--- run.py
+++ run.py
@@ -76,8 +76,13 @@ def calculate_surplus_data(sales_row):
 """
     print("Calculate  surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
-    stock_row = stock[8]
-    print(stock_row)
+    stock_row = stock[-1]
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    print(surplus_data)
 
 
 
